e0740c4f223b7d384de17a96595aba550839bc39
jjosecodes <josejob123121@gmail.com>
2025-04-14 11:21:31 -0500

    palintrome python

--- basics/fraction_class.py
+++ basics/fraction_class.py
@@ -7,7 +7,13 @@ class fraction:
     def __init__(self, top, bottom):
         self.num = top
         self.den = bottom
+# cant directly print fraction class 
+# since it gives the reference to the object
+    def show(self):
+        print(f"{self.num}/{self.den}")
+
 
 
 my_fraction = fraction(3, 5)
 print(my_fraction)
+my_fraction.show()
